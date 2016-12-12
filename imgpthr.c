d2cfb360287ee6c6e309780bc49fe918d8e1b14b
ruxandraS <ruxandra.simion@stud.acs.upb.ro>
2016-12-12 18:31:02 +0200

    Add pthreads implementation

--- imgpthr.c
+++ imgpthr.c
@@ -1,13 +1,13 @@
 #include "img.h"
 #include <pthread.h>
 
-#define NUM_THREADS 4
+int num_threads;
 
-typedef struct thread_info {    /* Used as argument to thread_start() */
-    pthread_t   thread_id;        /* ID returned by pthread_create() */
-    int         thread_num;       /* Application-defined thread # */
-    int         lw, lh, rw, rh;
-    image       *img;      
+typedef struct thread_info {
+    pthread_t   thread_id;          /* ID returned by pthread_create() */
+    int         thread_num;         /* application-defined thread # */
+    int         lw, lh, rw, rh;     /* chunk image left and right corners */
+    image       *in_img, *out_img;  /* input and output image */
 } thread_info;
 
 static
@@ -25,62 +25,42 @@ pixel pixel_reduce(pixel pixel)
     return 0;
 }
 
-static image *
-image_posterize(image *img)
+static void
+image_posterize(thread_info *tinfo)
 {
     int w, h;
-    image *posterized;
 
-    /* alloc memory for image */
-    posterized = image_new(img->width, img->height);
-
-    /* complete image header */
-    strcpy(posterized->type, img->type);
-    posterized->width = img->width;
-    posterized->height = img->height;
-    posterized->maxval = img->maxval;
-
-    /* complete image pixels */
-    for (h = 0; h < posterized->height; h++)
+    /* loop through chunk image and complete final image pixels */
+    for (h = tinfo->lh; h < tinfo->rh; h++)
     {
-        for (w = 0; w < posterized->width; w++)
+        for (w = tinfo->lw; w < tinfo->rw; w++)
         {
-            posterized->pix[h][w].red = pixel_reduce(img->pix[h][w].red);
-            posterized->pix[h][w].green = pixel_reduce(img->pix[h][w].green);
-            posterized->pix[h][w].blue = pixel_reduce(img->pix[h][w].blue);
+            tinfo->out_img->pix[h][w].red = pixel_reduce(tinfo->in_img->pix[h][w].red);
+            tinfo->out_img->pix[h][w].green = pixel_reduce(tinfo->in_img->pix[h][w].green);
+            tinfo->out_img->pix[h][w].blue = pixel_reduce(tinfo->in_img->pix[h][w].blue);
 
         }
     }
-
-    return posterized;
 }
 
 static void *
 thread_posterize (void * arg)
 {
     thread_info *tinfo = arg;
-    return image_posterize(tinfo->img);
+    image_posterize(tinfo);
+    return NULL;
+
 }
 
-static image *
-image_pixelate(image *img)
+static void
+image_pixelate(thread_info *tinfo)
 {
     int w, h, pw, ph, avg_red, avg_green, avg_blue, pixel_count;
-    image *pixelated;
 
-    /* alloc memory for image */
-    pixelated = image_new(img->width, img->height);
-
-    /* complete image header */
-    strcpy(pixelated->type, img->type);
-    pixelated->width = img->width;
-    pixelated->height = img->height;
-    pixelated->maxval = img->maxval;
-
-    /* loop through image matrix and determine pixel color for submatrixes */
-    for (h = 0; h < pixelated->height; h += PIXELATE_RATIO)
+    /* loop through chunk image matrix and determine pixel color for submatrixes */
+    for (h = tinfo->lh; h < tinfo->rh; h += PIXELATE_RATIO)
     {
-        for (w = 0; w < pixelated->width; w+= PIXELATE_RATIO)
+        for (w = tinfo->lw; w < tinfo->rw; w+= PIXELATE_RATIO)
         {
             /* new submatrix region - compute value of pixels */
             if (h % PIXELATE_RATIO == 0 && w % PIXELATE_RATIO == 0) {
@@ -90,12 +70,12 @@ image_pixelate(image *img)
                 pixel_count = 0;
 
                 for (ph = h; ph < h + PIXELATE_RATIO 
-                    && ph < pixelated->height; ph++) {
+                    && ph < tinfo->out_img->height; ph++) {
                     for (pw = w; pw < w + PIXELATE_RATIO 
-                        && pw < pixelated->width; pw++) {
-                        avg_red += img->pix[ph][pw].red;
-                        avg_green += img->pix[ph][pw].green;
-                        avg_blue += img->pix[ph][pw].blue;
+                        && pw < tinfo->out_img->width; pw++) {
+                        avg_red += tinfo->in_img->pix[ph][pw].red;
+                        avg_green += tinfo->in_img->pix[ph][pw].green;
+                        avg_blue += tinfo->in_img->pix[ph][pw].blue;
                         pixel_count++;
                     }
                 }
@@ -105,147 +85,198 @@ image_pixelate(image *img)
                 avg_blue /= pixel_count;
             }
 
-            /* fill in image pixels */
+            /* fill in final image pixels */
             for (ph = h; ph < h + PIXELATE_RATIO 
-                && ph < pixelated->height; ph++) {
+                && ph < tinfo->out_img->height; ph++) {
                 for (pw = w; pw < w + PIXELATE_RATIO 
-                    && pw < pixelated->width; pw++) {
-                    pixelated->pix[ph][pw].red = avg_red;
-                    pixelated->pix[ph][pw].green = avg_green;
-                    pixelated->pix[ph][pw].blue = avg_blue;
+                    && pw < tinfo->out_img->width; pw++) {
+                    tinfo->out_img->pix[ph][pw].red = avg_red;
+                    tinfo->out_img->pix[ph][pw].green = avg_green;
+                    tinfo->out_img->pix[ph][pw].blue = avg_blue;
                 }
-            }                    
+            }
         }
     }
+}
 
-    return pixelated;
+static void *
+thread_pixelate (void * arg)
+{
+    thread_info *tinfo = arg;
+    image_pixelate(tinfo);
+    return NULL;
+
 }
 
 int main(int argc, char const *argv[])
 {
     const char *filter, *filein, *fileout;
-    double start, end;
-    int h, w, hmax, wmax, ph, pw;
-    image *img, *posterized, *pixelated;
+    struct timespec start, end;
+    int hchunk, wchunk;
+    image *img, *out_img;
     long t;
     thread_info *tinfo;
     void *res;
 
-    if (argc < 4) {
-        printf("Usage: <executable> <filter_name> <input_file> <output_file>\n");
+    if (argc < 5) {
+        printf("Usage: <executable> <filter_name> <input_file> <output_file> <num_threads>\n");
         return 0;
     }
 
     filter = argv[1];
     filein = argv[2];
     fileout = argv[3];
+    num_threads = atoi(argv[4]);
 
     img = image_read(filein);
 
-    tinfo = calloc(NUM_THREADS, sizeof(thread_info));
+    tinfo = calloc(num_threads, sizeof(thread_info));
 
     /* apply posterize or pixelate filter to image */
     if (strcmp("pixelate", filter) == 0)
     {
-        start = clock();
-        pixelated = image_pixelate(img);
-        end = clock();
+        clock_gettime (CLOCK_MONOTONIC, &start);
+         out_img = image_new(img->width, img->height);
 
-        image_write(pixelated, fileout);
-        image_free(pixelated, pixelated->height);
-    }
+        /* complete chunk image header */
+        strcpy(out_img->type, img->type);
+        out_img->width = img->width;
+        out_img->height = img->height;
+        out_img->maxval = img->maxval;
 
-    else if (strcmp("posterize", filter) == 0)
-    {
-        /* compute dimension of chunks */
-        ph = img->height / NUM_THREADS;
-        pw = img->width / NUM_THREADS;
-
-        start = clock();
-
-        /* create NUM_THREADS threads */
-        for (t = 0; t < NUM_THREADS; t++)
+        /* create num_threads threads */
+        for (t = 0; t < num_threads; t++)
         {
             tinfo[t].thread_num = t + 1;
 
             /* send more pixels to last thread if height or width are not
-               perfectly divisable to NUM_THREADS */
-            if (t + 1 == NUM_THREADS) {
-                hmax = img->height;
-                wmax = img->width;
-            }
+               perfectly divisable to num_threads */
+            if (t + 1 == num_threads) {
+                hchunk = img->height;
+                wchunk = img->width;
 
-            else {
-                hmax = (t + 1) * ph;
-                wmax = (t + 1) * pw;
-            }
+                /* store top left corner and bottom right corner of each chunk */
+                if (t > 1) {
+                    tinfo[t].lh = tinfo[t - 1].lh;
+                    tinfo[t].lw = tinfo[t - 1].lw;
+                    tinfo[t].rh = hchunk;
+                    tinfo[t].rw = wchunk;
+                }
 
-            /* store top left corner and bottom right corner of each chunk */
-            tinfo[t].lh = hmax - t * ph;
-            tinfo[t].lw = wmax - t * pw;
-            tinfo[t].rh = hmax;
-            tinfo[t].rw = wmax;
-
-            /* create chunk image for further processing */
-            tinfo[t].img = image_new(pw, ph);
-
-            /* complete chunk image header */
-            strcpy(tinfo[t].img->type, img->type);
-            tinfo[t].img->width = wmax - t * pw;
-            tinfo[t].img->height = hmax - t * ph;
-            tinfo[t].img->maxval = img->maxval;
-
-            /* fill in chunk image pixels */
-            for (h = t * ph; h < hmax; h++)
-            {
-                for (w = t * pw; w < wmax; w++)
-                {
-                    tinfo[t].img->pix[h][w] = img->pix[h][w];
+                else {
+                    tinfo[t].lh = 0;
+                    tinfo[t].lw = 0;
+                    tinfo[t].rh = hchunk;
+                    tinfo[t].rw = wchunk;
                 }
             }
 
+            else {
+                hchunk = img->height / num_threads;
+                while (hchunk % PIXELATE_RATIO != 0)
+                    hchunk++;
+                wchunk = img->width;
+
+                /* store top left corner and bottom right corner of each chunk */
+                tinfo[t].lh = t * hchunk;
+                tinfo[t].lw = 0;
+                tinfo[t].rh = (t + 1) * hchunk;
+                tinfo[t].rw = wchunk;
+            }
+
+            tinfo[t].in_img = img;
+            tinfo[t].out_img = out_img;
+
+
             /* send chunks to each thread */
-            pthread_create(&tinfo[t].thread_id, 
+            pthread_create(&tinfo[t].thread_id,
+                           NULL,
+                           &thread_pixelate,
+                           &tinfo[t]);
+        }
+
+
+        /* gather processed chunk images from all threads */
+        for (t = 0; t < num_threads; t++) {
+            pthread_join(tinfo[t].thread_id, &res);
+        }
+
+        clock_gettime (CLOCK_MONOTONIC, &end);
+
+        image_write(out_img, fileout);
+    }
+
+    else if (strcmp("posterize", filter) == 0)
+    {
+        clock_gettime (CLOCK_MONOTONIC, &start);
+
+        /* create chunk image for further processing */
+        out_img = image_new(img->width, img->height);
+
+        /* complete chunk image header */
+        strcpy(out_img->type, img->type);
+        out_img->width = img->width;
+        out_img->height = img->height;
+        out_img->maxval = img->maxval;
+
+        hchunk = img->height / num_threads;
+        wchunk = img->width;
+
+        /* create num_threads threads */
+        for (t = 0; t < num_threads; t++)
+        {
+            tinfo[t].thread_num = t + 1;
+
+            /* send more pixels to last thread if height or width are not
+               perfectly divisable to num_threads */
+            if (t + 1 == num_threads) {
+                hchunk = img->height;
+                wchunk = img->width;
+
+                /* store top left corner and bottom right corner of each chunk */
+                if (t > 1) {
+                    tinfo[t].lh = tinfo[t - 1].lh;
+                    tinfo[t].lw = tinfo[t - 1].lw;
+                    tinfo[t].rh = hchunk;
+                    tinfo[t].rw = wchunk;
+                }
+
+                else {
+                    tinfo[t].lh = 0;
+                    tinfo[t].lw = 0;
+                    tinfo[t].rh = hchunk;
+                    tinfo[t].rw = wchunk;
+                }
+            }
+
+            else {
+                /* store top left corner and bottom right corner of each chunk */
+                tinfo[t].lh = t * hchunk;
+                tinfo[t].lw = 0;
+                tinfo[t].rh = (t + 1) * hchunk;
+                tinfo[t].rw = wchunk;
+            }
+
+            tinfo[t].in_img = img;
+            tinfo[t].out_img = out_img;
+
+
+            /* send chunks to each thread */
+            pthread_create(&tinfo[t].thread_id,
                            NULL,
                            &thread_posterize,
                            &tinfo[t]);
         }
 
-        /* create final image */
-        posterized = image_new(img->width, img->height);
-
-        /* complete image header */
-        strcpy(pixelated->type, img->type);
-        pixelated->width = img->width;
-        pixelated->height = img->height;
-        pixelated->maxval = img->maxval;
 
         /* gather processed chunk images from all threads */
-        for (t = 0; t < NUM_THREADS; t++) {
+        for (t = 0; t < num_threads; t++) {
             pthread_join(tinfo[t].thread_id, &res);
-
-            ph = 0; /* iterate through final image's height */
-            pw = 0; /* iterate through final image's width */
-
-            for (h = tinfo[t].lh; h < tinfo[t].rh; h ++)
-            {
-                for (w = tinfo[t].lw; w < tinfo[t].rw; w++)
-                {
-                    pixelated[h][w] = (image*)res.pix[ph][pw];
-                    pw++;
-
-                    if (pw == (image*)res.width) {
-                        ph++;
-                        pw = 0;
-                    }
-                }
-            }
         }
 
-        end = clock();
+        clock_gettime (CLOCK_MONOTONIC, &end);
 
-        image_write(posterized, fileout);
-        image_free(posterized, posterized->height);
+        image_write(out_img, fileout);
     }
 
     else
@@ -255,9 +286,12 @@ int main(int argc, char const *argv[])
 
     /* clean up */
     image_free(img, img->height);
+    image_free(out_img, out_img->height);
+    free(tinfo);
 
     /* determine serial time for later comparison */
-    printf("Pthreads running time is: %f\n", (end-start)/CLOCKS_PER_SEC);
+    printf("Pthreads running time for %d threads is: %f\n", num_threads,
+            end.tv_sec - start.tv_sec + (end.tv_nsec - start.tv_nsec) / 1000000000.0);
 
     return 0;
 }
